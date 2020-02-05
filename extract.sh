e3aae6351589fab869bc1c7bbc53ffb4e1d2ce53
Jacob Rutherford <Jacob.Rutherford@garmin.com>
2020-02-05 13:11:06 -0600

    extract.sh: Cleanup code and use proper sh
    I have no idea what was going through my head when I didn't just loop
    over the small set of files that I knew this repo consisted of and then
    made one exception for the lone directory.
    
    In the future if more directories are added then they can also be added
    to and checked for in the loop.

--- extract.sh
+++ extract.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-user_sh=$(getent passwd $USER | sed 's/.*://')
-case "$user_sh" in
+user_sh=$(getent passwd "${USER}" | sed 's/.*://')
+case "${user_sh}" in
     *"zsh")
-        echo "Default shell is $user_sh" 
+        echo "Default shell is ${user_sh}"
         ;;
 
     *)  # Interesting note, BSD is smart enough to figure out which
@@ -12,67 +12,40 @@ case "$user_sh" in
         # but on Linux you need to give chsh the full path to the shell
         # you want
         zsh=$(grep zsh /etc/shells | head -n1)
-        echo "Default shell is $user_sh" 
-        echo "Setting \$SHELL to $zsh"
-        chsh -s $zsh
+        echo "Default shell is ${user_sh}"
+        echo "Setting \$SHELL to ${zsh}"
+        chsh -s "${zsh}"
         ;;
 esac
 
-if [ ! -d $HOME/.oh-my-zsh ] ; then
+if [ ! -d "${HOME}/.oh-my-zsh" ] ; then
     echo "installing oh-my-zsh"
     sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
 
-repo_path=$(dirname $(readlink -f $0))
+repo_path=$(dirname "$(readlink -f "$0")")
 
-if [ -e $HOME/.zshrc ] ; then
-    mv $HOME/.zshrc $HOME/.old.zshrc
+for file in .zshrc .zsh.aliaes .zsh.exports .vimrc .tmux.conf ; do
+    if [ -e "${HOME}/${file}" ] ; then
+        mv "${HOME}/${file}" "${HOME}/${file}.old"
+    fi
+    echo "copying ${file} to ${HOME}/${file}"
+    cp "${repo_path}/${file}" "${HOME}/${file}"
+done
+
+if [ -d "${HOME}/.vim" ] ; then
+    mv "${HOME}/.vim" "${HOME}/.vim.old"
 fi
 
-echo copying $repo_path/.zshrc to $HOME/.zshrc
-cp $repo_path/.zshrc $HOME/.zshrc
-
-if [ -e $HOME/.zsh.aliases ] ; then
-    mv $HOME/.zsh.aliases $HOME/.old.zsh.aliases
-fi
-
-echo copying $repo_path/.zsh.aliases to $HOME/.zsh.aliases
-cp $repo_path/.zsh.aliases $HOME/.zsh.aliases
-
-if [ -e $HOME/.zsh.exports ] ; then
-    mv $HOME/.zsh.exports $HOME/.old.zsh.exports
-fi
-
-echo copying $repo_path/.zsh.exports to $HOME/.zsh.exports
-cp $repo_path/.zsh.exports $HOME/.zsh.exports
-
-if [ -e $HOME/.vimrc ] ; then
-    mv $HOME/.vimrc $HOME/.old.vimrc
-fi
-
-echo copying $repo_path/.vimrc to $HOME/.vimrc
-cp $repo_path/.vimrc $HOME/.vimrc
-
-if [ -e $HOME/.tmux.conf ] ; then
-    mv $HOME/.tmux.conf $HOME/.old.tmux.conf
-fi
-
-echo copying $repo_path/.tmux.conf to $HOME/.tmux.conf
-cp $repo_path/.tmux.conf $HOME/.tmux.conf
-
-if [ -d $HOME/.vim ] ; then
-    mv $HOME/.vim $HOME/.old.vim
-fi
-
-echo copying $repo_path/.vim to $HOME/.vim
-cp -r $repo_path/.vim $HOME/.vim
+echo "copying ${repo_path}/.vim to ${HOME}/.vim"
+cp -r "${repo_path}/.vim" "${HOME}/.vim"
 
 # install vim plugins using Plug
 vim +PlugInstall +qall
 
-upstream=$HOME/upstream
-if [ ! -d ${upstream} ] ; then
-  mkdir -p ${upstream}
+upstream="${HOME}/upstream"
+if [ ! -d "${upstream}" ] ; then
+  mkdir -p "${upstream}"
 fi
 echo "Cloning Zeta theme to ${upstream}/zeta-zsh-theme"
 mkdir ${upstream}/zeta-zsh-theme
